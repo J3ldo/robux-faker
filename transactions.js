ea8af5c21ec8d0c3fcda91431377a9083ea8745e
J3ldo <97303347+J3ldo@users.noreply.github.com>
2023-07-28 02:18:15 +0200

    More options

--- transactions.js
+++ transactions.js
@@ -6,30 +6,51 @@ chrome.storage.sync.get("fullrobux", function(items){
     let fullRobux = items["fullrobux"];
 })
 
+function setTotal() {
+    for (i=0; i<6; i++){
+        total += parseInt(element[i].children[2].innerHTML.split('.').join("").split(",").join(""))
+        if (i == 5)
+            continue;
+    }
+    totalstr = total.toLocaleString("en-150");
+    loaded = true;
+    element[7].children[2].innerHTML = totalstr
+}
+
 
 function OnBalanceExist(){
     if (document.getElementsByClassName("balance-label icon-robux-container")[0]
     && document.getElementsByClassName("balance-label icon-robux-container")[0].children[0].innerHTML != `<span class="shimmer-line"></span>`){
         document.getElementsByClassName("balance-label icon-robux-container")[0].children[0].innerHTML = `My Balance: <span class="icon-robux-16x16"></span>${fullRobux}`;
-
+        
         chrome.storage.sync.get("incoming", function(items){
-            total = 0;
             element = document.getElementsByClassName("amount icon-robux-container");
+            total = 0;
         
             incoming = items["incoming"];
-            element[2].children[2].innerHTML = incoming;
-
+            element[2].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${incoming}</span>`;
             
-            for (i=0; i<6; i++){
-                total += parseInt(element[i].children[2].innerHTML.split('.').join("").split(",").join(""))
-                if (i == 5)
-                    continue;
-            }
-            totalstr = total.toLocaleString("US-en");
-            loaded = true;
+            setTotal();
+        })
 
-	    if (element.length == 9) element[6].children[2].innerHTML = totalstr;
-            else element[7].children[2].innerHTML = totalstr;
+        chrome.storage.sync.get("pending", function(items){
+            element = document.getElementsByClassName("amount icon-robux-container");
+            total = 0;
+        
+            pending = items["pending"];
+            element[5].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${pending}</span>`;
+            
+            setTotal();
+        })
+
+        chrome.storage.sync.get("bought", function(items){
+            element = document.getElementsByClassName("amount icon-robux-container");
+            total = 0;
+        
+            bought = items["bought"];
+            element[1].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${bought}</span>`;
+            
+            setTotal();
         })
     }
     else {
@@ -45,20 +66,11 @@ function OnBalanceChange(){
         
         element[2].innerHTML = `<span></span><span class="icon-robux-16x16"></span><span>${incoming}</span>`;
             
-        for (i=0; i<6; i++){
-            total += parseInt(element[i].children[2].innerHTML.split('.').join("").split(",").join(""))
-            if (i == 5)
-                continue;
-        }
-        totalstr = total.toLocaleString("US-en");
-        loaded = true;
-        
-        if (element.length == 9) element[6].children[2].innerHTML = totalstr;
-        else element[7].children[2].innerHTML = totalstr;
+        setTotal()
     }
     
     else {
-        setTimeout(OnBalanceChange, 10);
+        setTimeout(OnBalanceChange, 1);
     }
 }
 
