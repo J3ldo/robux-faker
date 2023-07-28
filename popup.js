930ea0a7d0e4e527216375a97ad39b581f0c4780
J3ldo <97303347+J3ldo@users.noreply.github.com>
2023-07-28 02:16:13 +0200

    More options

--- popup.js
+++ popup.js
@@ -27,3 +27,23 @@ chrome.storage.sync.get("incoming", function(items){
 document.getElementById("incoming").addEventListener("input", function(evt){
     chrome.storage.sync.set({"incoming": document.getElementById("incoming").value});
 })
+
+chrome.storage.sync.get("pending", function(items){
+    if (items["pending"] == undefined) return;
+
+    document.getElementById("pending").value = items["pending"];
+});
+
+document.getElementById("pending").addEventListener("input", function(evt){
+    chrome.storage.sync.set({"pending": document.getElementById("pending").value});
+})
+
+chrome.storage.sync.get("bought", function(items){
+    if (items["bought"] == undefined) return;
+
+    document.getElementById("bought").value = items["bought"];
+});
+
+document.getElementById("bought").addEventListener("input", function(evt){
+    chrome.storage.sync.set({"bought": document.getElementById("bought").value});
+})
